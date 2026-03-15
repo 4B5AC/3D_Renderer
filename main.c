cb9bb449e646069ac2571b5cc3229e4df5dfc482
4B5AC <4b5a0b@gmail.com>
2026-03-15 13:07:56 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,64 @@
 #include "SDL3/SDL.h"
 #include "SDL3/SDL_main.h"
 #include <stdio.h>
+#include "math.h"
 
 #define WIDTH 800
 #define HEIGHT 600
+
+#define VERTEX_COUNT 100000
+
+#define step 3
 
 struct Vector3 {
     float x,y,z;
 };
 
+
 struct Camera {
     struct Vector3 pos;
     float focalLen;
 };
+
+struct Matrix3X3 {
+    float a,b,c,d,e,f,g,h,i;
+};
+
+void rotateY(struct Vector3* vector, float angleDegrees){
+    float rad = angleDegrees * (3.14 / 180.0f);
+
+    struct Matrix3X3 matrix = {
+        cosf(rad), 0, sinf(rad),
+        0,         1, 0,
+       -sinf(rad), 0, cosf(rad)
+    };
+
+    float x = vector->x;
+    float y = vector->y;
+    float z = vector->z;
+
+    vector->x = matrix.a * x + matrix.b * y + matrix.c * z;
+    vector->y = matrix.d * x + matrix.e * y + matrix.f * z;
+    vector->z = matrix.g * x + matrix.h * y + matrix.i * z;
+}
+
+void rotateX(struct Vector3* vector, float angle){
+    float rad = angle * (3.14 / 180.0f);
+
+    struct Matrix3X3 matrix = {
+        1,       0,        0,
+        0,  cosf(rad), -sinf(rad),
+        0,  sinf(rad),  cosf(rad)
+    };
+
+    float x = vector->x;
+    float y = vector->y;
+    float z = vector->z;
+
+    vector->x = matrix.a * x + matrix.b * y + matrix.c * z;
+    vector->y = matrix.d * x + matrix.e * y + matrix.f * z;
+    vector->z = matrix.g * x + matrix.h * y + matrix.i * z;
+}
 
 struct Vector3 translate(struct Vector3* vector,float x,float y,float z){
     struct Vector3 translated;
@@ -64,7 +110,7 @@ size_t get_vertices(const char* fileName,struct Vector3* buffer,size_t max){
 
     size_t count = 0;
     char line[256];
-    while(fgets(line,sizeof(line),file)){
+    while(fgets(line,sizeof(line),file) && count<max){
         if(startsWith(line,"v ")){
             float x,y,z;
             if(sscanf(line,"v %f %f %f",&x,&y,&z)==3){
@@ -85,8 +131,7 @@ void draw_vertices(SDL_Renderer* renderer,struct Vector3* arr,struct Camera* cam
     }
 }
 
-void handle_movement(const bool* keystate,struct Camera* cam){
-    float step = 0.05f;
+void handle_keyboard(const bool* keystate,struct Camera* cam,size_t vertex_count,struct Vector3* vertices){
     if(keystate[SDL_SCANCODE_A]){
         cam->pos.x -= step;
     }else if(keystate[SDL_SCANCODE_D]){
@@ -99,12 +144,20 @@ void handle_movement(const bool* keystate,struct Camera* cam){
         cam->pos.y += step;
     }else if(keystate[SDL_SCANCODE_Q]){
         cam->pos.y -= step;
+    }else if(keystate[SDL_SCANCODE_R]){
+        for(size_t i=0;i<vertex_count;i++){
+            rotateY(&vertices[i],5);
+        }
+    }else if(keystate[SDL_SCANCODE_T]){
+        for(size_t i=0;i<vertex_count;i++){
+            rotateX(&vertices[i],5);
+        }
     }
 }
 
 int main(int argc,char* argv[]){
-    struct Vector3 vertices[256];
-    size_t vertexCount = get_vertices("model.obj",vertices,256);
+    struct Vector3 vertices[VERTEX_COUNT];
+    size_t vertexCount = get_vertices("model.obj",vertices,VERTEX_COUNT);
 
     struct Camera userCamera = {{0,0,-10},900.0};
 
@@ -129,7 +182,7 @@ int main(int argc,char* argv[]){
         }
 
         const bool* keystate = SDL_GetKeyboardState(NULL);
-        handle_movement(keystate,&userCamera);
+        handle_keyboard(keystate,&userCamera,vertexCount,vertices);
         
         SDL_SetRenderDrawColor(renderer,0,0,0,255);
         SDL_RenderClear(renderer);
